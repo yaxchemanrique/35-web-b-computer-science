349d274b5373326b5bf95c06aa6c166437a54e81
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-07-04 20:02:05 -0700

    feat: solucion clases es06 - 2

--- 3-poo/3.2.1-ejercicio-calses-es06.js
+++ 3-poo/3.2.1-ejercicio-calses-es06.js
@@ -9,4 +9,31 @@
   * Esta debe de ser una funcion que regrese true en caso de ser un correo valido y false en caso de no serlo.
 */
 
+class User {
+  constructor(username, password, email, isOnline){
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.isOnline = isOnline;
+  }
+
+  isValid() {
+    // if(this.email.includes('@')) {
+    // if(this.email.indexOf('@') > -1) {
+    // if(this.email.indexOf('@') !== -1) {
+    //   return true;
+    // }
+    // return false;
+
+    return this.email.includes('@');
+  }
+}
+
+
 //usuario1.validEmail() // true o false
+
+const user1 = new User("yaxche", "password", "yaxche@mail.com", true);
+console.log(user1)
+
+const user2 = new User("Susana", "contraseña", "susanamail.com", true);
+console.log(user2)
