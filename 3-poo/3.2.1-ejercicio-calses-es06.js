b34f6d191b53cab2a5b543fbd272d835552eda5b
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-07-04 18:57:36 -0700

    feat: ejercicio clases es06

---
+++ 3-poo/3.2.1-ejercicio-calses-es06.js
@@ -0,0 +1,12 @@
+/* 
+* Crea una clase para un usuario (User). Los usuarios deben de tener las propiedaddes de:
+  * username
+  * password
+  * email
+  * isOnline
+
+* Se debe de validar que los usuarios tengan un correo valido (en este caso que contenga un arroba @)
+  * Esta debe de ser una funcion que regrese true en caso de ser un correo valido y false en caso de no serlo.
+*/
+
+//usuario1.validEmail() // true o false
